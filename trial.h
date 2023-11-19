196d5c1d3e2611526d558c1f4583dd769b8cc337
Arjun Menon <83897925+arjunmenon109@users.noreply.github.com>
2023-11-19 13:51:16 +0530

    Create trial.h

---
+++ trial.h
@@ -0,0 +1,33 @@
+5.5 Key course(s) in mathematics and programming
+
+
+URL to the entire course curriculum: https://www.bennett.edu.in/wp-content/uploads/2023/10/ME_2020-24-batch_CURRICULUM-WITH-DETAILED-SYLLABUS_Final_compressed.pdf 
+
+Sem 1:
+EMAT101L; Engineering Calculus; Local Credits: 4; Local Grade: D (Mathematics)
+ECSE105L; Computational Thinking & Programming; Local Credits: 5; Local Grade: B (Programming)
+Sem2:
+EMAT102L; Linear Algebra and Ordinary Differential Equations; Local Credits: 4; Local Grade: B; (Mathematics)
+Sem3:
+EMAT205L; Complex Analysis and Partial Differential Equations; Local Credits: 3; Local Grade: B; (Mathematics)
+Sem4:
+EMAT202L; Numerical Methods; Local Credits: 3; Local Grade: B; (Mathematics)
+Sem5: 
+CSET389; Linux Apache MySql PHP (LAMP); Local Credits: 3; Local Grade: C+ (Programming)
+
+
+
+5.6.1 Course(s) in Solid mechanics and Fluid mechanics
+
+URL to the entire course curriculum: https://www.bennett.edu.in/wp-content/uploads/2023/10/ME_2020-24-batch_CURRICULUM-WITH-DETAILED-SYLLABUS_Final_compressed.pdf 
+
+SEM 2:
+EMEC108L; Engineering Statics and Dynamics; Local Credits: 4; Local Grade: B+
+
+SEM 3:
+EMEC209L; Solid Mechanics; Local Credits: 4; Local Grade: A
+EMEC206L; Fluid Mechanics and Machines; Local Credits: 5; Local Grade: B+
+
+SEM 4:
+EMEC212L; Finite Element Method; Local Credits:3 ; Local Grade: A
+EMEC208L; Kinematics and Dynamics of Machines; Local Credits: 5; Local Grade: B
